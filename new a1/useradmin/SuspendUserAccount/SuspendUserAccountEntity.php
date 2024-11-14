95660ad093c7fe090d17a3b29e6c71ebd3921ebb
applezxc <nicolasng99@gmail.com>
2024-11-14 18:56:18 +0800

    Fix some UI typos and misc edits

--- new a1/useradmin/SuspendUserAccount/SuspendUserAccountEntity.php
+++ new a1/useradmin/SuspendUserAccount/SuspendUserAccountEntity.php
@@ -11,7 +11,7 @@
 
 
         public function suspendUserAccount($userId){
-            $stmt = $this->conn->prepare("UPDATE useraccount SET status = 'Suspend' where id = ? ");
+            $stmt = $this->conn->prepare("UPDATE useraccount SET status = 'Suspended' where id = ? ");
             $stmt->bind_param("i",$userId);
             
             if($stmt->execute()){
