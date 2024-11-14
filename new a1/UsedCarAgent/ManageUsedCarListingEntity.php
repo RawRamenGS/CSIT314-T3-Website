42beafb48e93f80435cb8fb4a71ff559a26d3665
Bob JackFroots <73485399+xzyrynthz@users.noreply.github.com>
2024-11-15 05:13:30 +0800

    Fix carID not parsed

--- new a1/UsedCarAgent/ManageUsedCarListingEntity.php
+++ new a1/UsedCarAgent/ManageUsedCarListingEntity.php
@@ -13,7 +13,7 @@ class ManageUsedCarListingEntity{
 
     // Static method to get the total number of cars (for pagination)
     public function getfavcar(){
-        $stmt = $this->conn->prepare("SELECT c.carName, u.username, c.price, c.favourites, c.views FROM carlisting c INNER JOIN useraccount u ON c.agent = u.id WHERE u.profileId = 2;");
+        $stmt = $this->conn->prepare("SELECT c.carID.carName, u.username, c.price, c.favourites, c.views FROM carlisting c INNER JOIN useraccount u ON c.agent = u.id WHERE u.profileId = 2;");
         $stmt->execute();
         $result = $stmt->get_result();
 
