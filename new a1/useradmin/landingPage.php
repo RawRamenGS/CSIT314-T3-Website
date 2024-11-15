e64b3a32144baeb8b740fdb1ee4f75368aa1987d
RawRamenGS <limguoshun@gmail.com>
2024-11-15 15:43:50 +0800

    Text Fix for Admin Landing Page

--- new a1/useradmin/landingPage.php
+++ new a1/useradmin/landingPage.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['is_logged_in']) || $_SESSION['is_logged_in'] !== true) {
 </head>
 <body>
     <header>
-        <h1>Website Title</h1>
+        <h1></h1>
         <div class="user-profile">
             <div class="profile-icon">&#128100;</div>
          <a href="../Login/Logout.php"><button class="logoutBtn">Logout</button></a>
